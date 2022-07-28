b9add7601af2f570c10bfae88e90009e45189c57
jhlee-danbi <jh.lee@danbicorp.com>
2022-07-28 21:16:24 +0900

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:my_scheduler/pages/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(fontFamily: 'NotoSans'), home: const HomeScreen()));
+}
